6c617471a4ce18d327c189ed60ee61223f93668c
Josh Marchand <josh.r.marchand@gmail.com>
2020-03-20 10:37:12 -0500

    java second

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+
+
+
+
+        double pi = 3.14159;
+
+//        int myFavoriteNumber = 7;
+//        String myString = "Josh";
+////        float myNumber = 3.14;
+//
+////        int x = 5;
+////        System.out.println(++x);
+////        System.out.println(x);
+////
+////        String theNumberThree = "three";
+////        Object o = theNumberThree;
+////        int three = (int) o;
+////        System.out.print(three);
+//
+//        int x = Integer.MAX_VALUE;
+//        System.out.println(x);
     }
 }
