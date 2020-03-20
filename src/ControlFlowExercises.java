54a2c4c13751a5e7860a11d61e4e8de9da12c253
Josh Marchand <josh.r.marchand@gmail.com>
2020-03-20 16:35:50 -0500

    4

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        long i = 5;
+//
+//        while (i <= 15){
+//            System.out.print(i++ + " ");
+//
+//        }
+//
+//       i = 2;
+//        do {
+//            System.out.println(i *= i);
+//        } while (i < 1000000);
+
+//        for (long i = 2; i < 1000000; ){
+//            System.out.println(i *= i);
+//        }
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+                System.out.print("Enter integer: ");
+        int userInput = scanner.nextInt();
+    }
+}
