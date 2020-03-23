48da1dfde18eb99cb47269a4724d19ee93a6b4ec
Josh Marchand <josh.r.marchand@gmail.com>
2020-03-23 10:46:59 -0500

    first

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("We don't need no education/n We don't need no thought control");
+    }
+}
